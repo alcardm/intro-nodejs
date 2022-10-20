3b375601c0a3be75db2dfa0de687742b6fb92a65
Alberto Cardenas <alcardm@gmail.com>
2022-10-20 11:43:18 -0500

    Completado ejercicio con API del clima y Axios.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-// Ver repositorio de la clase para complementar
-// https://github.com/anfejaramillo/intronodegp2
+const axios = require("axios");
+
 console.log("🖖");
+
+let apiKey = "d09b9795e856a1eeb8f8e90d3eb67840";
+let city = "Tunja, col";
+
+let requestConfig = {
+  url:
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey,
+};
+// console.log(requestConfig);
+
+let objPromesa = axios(requestConfig);
+
+objPromesa
+  .then(function (response) {
+    // Consultar la documentación de la API para revisar cuales
+    // está retornando la consulta
+    // https://openweathermap.org/current#name
+
+    // Debug
+    // console.log(response);
+    // console.log(response.data);
+    console.log(
+      "La temperatura en " +
+        city +
+        " es " +
+        (response.data.main.temp - 273.15).toFixed(2).toString() +
+        "°C."
+    );
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
